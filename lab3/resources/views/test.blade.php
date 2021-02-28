e6f166b8844888373495635e3a988f74368cda43
Daxak07 <zhuandyk.200@mail.ru>
2021-02-28 14:34:53 +0600

    create migrations

---
+++ lab3/resources/views/test.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Reverse telephone number: <?php echo $telephone_number; ?></h1>
+</body>
+</html>
--- lab3/routes/web.php
+++ lab3/routes/web.php
@@ -8,8 +8,7 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider within a group whichreate something great!
 |
 */
 
@@ -25,5 +24,25 @@ Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
 
+Route::get('/example', function () {
+	return "Web programming back end";
+});
 
+Route::get('hello', function () {
+	return redirect('/contact');
+});
+
+Route::redirect('salam', '/about');
+
+Route::get('/post/{id}', function ($id) {
+	return "id number is : " . $id;
+});
+
+Route::get('/user/{name?}', function ($name=null) {
+	return $name;
+})->where('name','[a-zA-Z]+');
+
+Route::get('/post', 'App\Http\Controllers\PostsController@index');
+
+Route::get('maaa/{tel}', 'App\Http\Controllers\PostsController@reverse_number');
 
